611ded637ab84ad3952a5215d4444c4483a65041
Narothe <kuba.dojka@gmail.com>
2024-05-07 08:20:29 +0200

    feat java 22

--- GrammarExpression/src/generated/Main.java
+++ GrammarExpression/src/generated/Main.java
@@ -1,15 +1,21 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Main {
     public static void main(String[] args) {
-        String tString = "d3 ^ (w2 V w3) (f2 V f3) (t3) (r3) (a3 V a4 V a5) ";
+        String tString = "d4 ^ (w2 V w3) (f2 V f3) (t2 V t3) (r2 V r3) (a2 V a3 V a4 V a5)";
         CharStream stream = CharStreams.fromString(tString);
         ExprLexer lexer = new ExprLexer(stream);
         ExprParser parser = new ExprParser(new org.antlr.v4.runtime.CommonTokenStream(lexer));
 
-        ExprParser.ProgramContext program = parser.program();
+        ExprParser.ProgramContext program = null;
+        try {
+            program = parser.program();
+        } catch (RecognitionException e) {
+            throw new RuntimeException(e);
+        }
 
         MyExprListener listener = new MyExprListener();
         ParseTreeWalker walker = new ParseTreeWalker();
